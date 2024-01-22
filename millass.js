1d7f80f902bf658bd1b2c1d293b9ad1ecb49d5f9
Daihana <coronelfatima94@gmail.com>
2024-01-22 11:35:24 -0300

    nuevas tareas

---
+++ millass.js
@@ -0,0 +1,4 @@
+const distanciaEnMillas = 130;
+const distanciaEnKilometros = distanciaEnMillas * 1.60934;
+
+console.log(`La distancia de ${distanciaEnKilometros.toFixed(4)} Km es igual a ${distanciaEnMillas} millas `);
