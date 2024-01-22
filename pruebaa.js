cc31587c2ad0e6b97c4d58b7fa316d1ac9345055
Daihana <coronelfatima94@gmail.com>
2024-01-22 13:18:48 -0300

    la prueba

---
+++ pruebaa.js
@@ -0,0 +1,66 @@
+// ¿Qué tipo de dato tiene la siguente variable?
+//const c = "5";
+String 
+
+//¿Qué tipo de dato tiene la siguente variable? const c = 91;
+numerico 
+
+//¿Cuál de los dos es mejor?, línea 1 o línea 2?
+//et empty1 = undefined; //line 1
+//let empty2 = null; //line 2
+ linea 2
+
+ //¿Qué salida produce en consola?
+//let a = "Hello";
+//a = "world";
+//console.log(a);
+Hello
+
+//¿Qué se mostrará en la consola?
+//let a = "world";
+//let b = `Hello ${a}!`;
+//console.log(b);
+Hello World
+
+//¿Cuál es el valor de a?
+//let a = "Hello";
+//a = prompt("world");
+//console.log(a);
+Hello 
+
+//¿Cuál es el valor de b en la consola?
+//let a = 5;
+//let b = 70;
+//let c = "5";
+//b++;
+//console.log(b);
+71
+
+//¿Cuál es el valor de este resultado? let result = 3 + 4 * 2 / 8;
+4
+
+//¿Cuál es el valor de total y total2?
+//let firstNum = 5;
+//let secondNum = 10;
+//firstNum++;
+//secondNum--;
+//let total = ++firstNum + secondNum;
+//console.log(total);
+//let total2 = 500 + 100 / 5 + total--;
+//console.log(total2);
+total = 17;
+total2 = 122;
+
+//¿Qué se mostrará en la consola aquí?
+//const a = 5;
+//const b = 10;
+//console.log(a > 0 && b > 0);
+//console.log(a == 5 && b == 4);
+//console.log(true ||false);
+//console.log(a == 3 || b == 10);
+//console.log(a == 3 || b == 7);
+true
+false
+true
+true
+false
