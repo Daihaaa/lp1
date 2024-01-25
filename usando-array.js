5912177615f42129d9442477d939d4dcdbb3867f
Daihana <coronelfatima94@gmail.com>
2024-01-25 13:23:54 -0300

    otratarea

---
+++ usando-array.js
@@ -0,0 +1,11 @@
+//variables normales y sus tipos
+let unaVariable = "Naranja";
+
+//arrays
+let razasDePerros = ["labrador", "gran Danes", "pitbull", "san bernardo", "pastor aleman"];
+console.log({razasDePerros});
+
+console.log(razasDePerros[4]);
+
+//uso de la propiedad lenght
+console.log(razasDePerros.length);
